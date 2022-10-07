11a99451329a1704c27dc55f646448d44ae24511
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-10-07 17:07:25 +0200

    Hitinfo

---
+++ Assets/Core/General/Models/HitInfo.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace GLShared.General.Models
+{
+	[System.Serializable]
+	public class HitInfo
+	{
+		public RaycastHit rayHit;
+		private float normalAndUpAngle;
+		public void CalculateNormalAndUpDifferenceAngle()
+        {
+			this.normalAndUpAngle = Vector3.Angle(Vector3.up, rayHit.normal);
+		}
+
+		public Vector3 Point
+		{
+			get => rayHit.point;
+		}
+		public Vector3 Normal
+		{
+			get => rayHit.normal;
+		}
+
+		public float Distance
+        {
+			get => rayHit.distance;
+        }
+
+		public Collider Collider
+        {
+			get => rayHit.collider;
+        }
+
+		public float NormalAndUpAngle
+        {
+			get => normalAndUpAngle;
+
+		}
+	}
+}

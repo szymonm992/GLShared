b0f266c7d4885d9a5e83238db853f5898f01fbd4
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-11-19 09:15:17 +0100

    extension return value

--- Assets/Core/General/Models/UTWheelBase.cs
+++ Assets/Core/General/Models/UTWheelBase.cs
@@ -198,6 +198,7 @@ namespace GLShared.General.Models
                 else
                 {
                     compressionRate = 1;
+                    extension = 0;
                 }
             }
             else

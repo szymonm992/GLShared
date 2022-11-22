dfe5f94f69e0332b3333e1c3d40b1d537497ab9a
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-11-22 04:10:57 +0100

    Idler wheel base

---
+++ Assets/Core/General/Components/UTIdlerWheel.cs
@@ -0,0 +1,11 @@
+using GLShared.General.Interfaces;
+using GLShared.General.Models;
+using Zenject;
+using UnityEngine;
+
+namespace GLShared.General.Components
+{
+    public class UTIdlerWheel : MonoBehaviour
+    {
+    }
+}

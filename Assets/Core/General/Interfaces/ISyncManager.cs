c52915e25d0fccb71c3ac078e289181a57be17fe
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-12-21 19:16:20 +0100

    isyncmanager

--- Assets/Core/General/Interfaces/ISyncManager.cs
+++ Assets/Core/General/Interfaces/ISyncManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 namespace GLShared.General.Interfaces
 {
-    public interface ISyncManager
+    public interface ISyncManager : IInitializable
     {
+        double CurrentServerTime { get; }
         int SpawnedPlayersAmount { get; }
 
         void CreatePlayer(bool isLocal, string vehicleName, Vector3 spawnPosition, Quaternion spawnRotation);

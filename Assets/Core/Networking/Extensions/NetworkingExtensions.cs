b26e2cf2221d3377164a4ef7d9063eec590a8699
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-12-23 19:25:41 +0100

    networking extensions

---
+++ Assets/Core/Networking/Extensions/NetworkingExtensions.cs
@@ -0,0 +1,25 @@
+using GLShared.General.Models;
+using GLShared.Networking.Models;
+using Sfs2X.Entities.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GLShared.Networking.Extensions
+{
+    public static class NetworkingExtensions
+    {
+       public static ISFSObject ToISFSOBject(this PlayerProperties properties)
+        {
+            ISFSObject data = new SFSObject();
+            data.PutUtfString("username", properties.User.Name);
+            data.PutFloat("spawnPositionX", properties.SpawnPosition.x);
+            data.PutFloat("spawnPositionY", properties.SpawnPosition.y);
+            data.PutFloat("spawnPositionZ", properties.SpawnPosition.z);
+            data.PutFloat("spawnRotationX", properties.SpawnRotation.eulerAngles.x);
+            data.PutFloat("spawnRotationY", properties.SpawnRotation.eulerAngles.y);
+            data.PutFloat("spawnRotationZ", properties.SpawnRotation.eulerAngles.z);
+            return data;
+        }
+    }
+}

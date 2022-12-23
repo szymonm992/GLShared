af0efbf1199bff7df072042ddf90fdced53b7b44
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-12-23 19:03:08 +0100

    some changes, serializable types only

--- Assets/Core/Networking/Models/NetworkPlayerProperties.cs
+++ Assets/Core/Networking/Models/NetworkPlayerProperties.cs
@@ -9,8 +9,26 @@ namespace GLShared.Networking.Models
 {
     public class NetworkPlayerProperties : SerializableSFSType
     {
-        public Vector3 SpawnPosition { get; set; }
-        public Quaternion SpawnRotation { get; set; }
-        public User User { get; set; }
+        public float SpawnPositionX { get; }
+        public float SpawnPositionY { get; }
+        public float SpawnPositionZ { get; }
+
+        public float SpawnEulerX { get; }
+        public float SpawnEulerY { get; }
+        public float SpawnEulerZ { get; }
+        public User User { get; }
+
+        public NetworkPlayerProperties(Vector3 spawnPosition, Quaternion spawnRotation, User user)
+        {
+            this.SpawnPositionX = spawnPosition.x;
+            this.SpawnPositionY = spawnPosition.y;
+            this.SpawnPositionZ = spawnPosition.z;
+
+            this.SpawnEulerX = spawnRotation.eulerAngles.x;
+            this.SpawnEulerY = spawnRotation.eulerAngles.y;
+            this.SpawnEulerZ = spawnRotation.eulerAngles.z;
+
+            this.User = user;
+        }
     }
 }

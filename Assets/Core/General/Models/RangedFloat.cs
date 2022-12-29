dcb657351ea7bfc29936e7a3c2db968b15bbf40c
Black heart <52672318+szymonm992@users.noreply.github.com>
2022-12-29 21:13:04 +0100

    some refactor

--- Assets/Core/General/Models/RangedFloat.cs
+++ Assets/Core/General/Models/RangedFloat.cs
@@ -5,38 +5,48 @@ namespace GLShared.General.Models
     [System.Serializable]
     public class RangedFloat
     {
+        [SerializeField] private float min;
+        [SerializeField] private float max;
+        
+        public float Max => max;
+        public float Min => min;
+        public float Difference => max - min;
+        public float Length => max - min;
+
         public RangedFloat(float min = float.NegativeInfinity, float max = float.PositiveInfinity)
         {
             this.min = min;
             this.max = max;
         }
-        public float min;
-        public float max;
 
-        public float difference => max - min;
+        public static bool InRange(float value, float min, float max)
+        {
+            return value >= min && value <= max;
+        }
 
-        public float length => max - min;
         public float GetPercent(float value)
         {
-            return (value - min) / length;
+            return (value - min) / Length;
+        }
+
+        public float GetMidPoint(float value)
+        {
+            return (min + max) * 0.5f;
         }
 
         public bool InRange(float value)
         {
             return value >= min && value <= max;
         }
+
         public float GetRandom()
         {
             return Random.Range(min, max);
         }
-      
-        public static bool InRange(float value, float min, float max)
+        
+        public float Clamp(float value)
         {
-            return value >= min && value <= max;
-        }
-        public float Clamp(float v)
-        {
-            return Mathf.Clamp(v, min, max);
+            return Mathf.Clamp(value, min, max);
         }
     }
 } 

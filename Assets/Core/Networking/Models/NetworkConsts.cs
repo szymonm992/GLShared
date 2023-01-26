f97046449c2dcc8e47c7a7d1d726c7d24a98c90d
Black heart <52672318+szymonm992@users.noreply.github.com>
2023-01-26 11:28:56 +0100

    network consts

---
+++ Assets/Core/Networking/Models/NetworkConsts.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GLShared.Networking.Models
+{
+    public static class NetworkConsts 
+    {
+        public const string VAR_BATTLE_TYPE = "battleType";
+        public const string VAR_PLAYER_VEHICLE = "playerVehicle";
+    }
+}

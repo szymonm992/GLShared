5705cc2cd89b2005901c79aafe8acab630558dce
Black heart <52672318+szymonm992@users.noreply.github.com>
2023-02-03 00:47:28 +0100

    identifier instead of properties identifier

--- Assets/Core/Networking/Models/SyncManagerBase.cs
+++ Assets/Core/Networking/Models/SyncManagerBase.cs
@@ -110,7 +110,7 @@ namespace GLShared.Networking.Models
             var shellEntity = shellSpawner.Spawn(prefabEntity, shellProperties);
 
             spawnedShellsAmount++;
-            shells.Add(shellProperties.Identifier, shellEntity);
+            shells.Add(identifier, shellEntity);
 
             Debug.Log($"Player {username} has shot a shell of id ({shellProperties.ShellId}) with network id ({shellProperties.Identifier})");
         }

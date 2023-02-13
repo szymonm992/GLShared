a812909fd344e6c96c72d054b6527c4335b17e95
Black heart <52672318+szymonm992@users.noreply.github.com>
2023-02-13 17:25:21 +0100

    Tank steering moved to shared repo

---
+++ Assets/Core/General/Components/UTTankSteering.cs
@@ -0,0 +1,86 @@
+using Frontend.Scripts.Interfaces;
+using GLShared.General.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace GLShared.General.Components
+{
+    public class UTTankSteering : MonoBehaviour, IVehicleSteering
+    {
+        [Inject(Id = "mainRig")] private Rigidbody rig;
+        [Inject] private readonly IVehicleController suspensionController;
+        [Inject] private readonly IPlayerInputProvider inputProvider;
+
+        [SerializeField] private float steerForce;
+
+        private float steerInput;
+        private float currentSteerForce;
+
+        public float SteerForce => steerForce;
+
+        public void SetSteeringInput(float input)
+        {
+            steerInput = inputProvider.AbsoluteVertical != 0 ? input * inputProvider.SignedVertical : input;
+        }
+
+        private void Update()
+        {
+            if (suspensionController != null)
+            {
+                SetSteeringInput(inputProvider.Horizontal);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (steerInput == 0 || suspensionController.IsUpsideDown)
+            {
+                return;
+            }
+
+            currentSteerForce = GetSteerForceMultiplier();
+
+            if (inputProvider.CombinedInput > 1f)
+            {
+                currentSteerForce *= (1.0f / Mathf.Sqrt(2));
+            }
+
+            if (!suspensionController.RunPhysics)
+            {
+                return;
+            }
+
+            foreach (var axle in suspensionController.AllAxles)
+            {
+                if (axle.CanSteer)
+                {
+                    var wheelsInAxle = axle.AllWheels;
+
+                    foreach (var wheel in wheelsInAxle)
+                    {
+                        int invertValue = axle.InvertSteer ? -1 : 1;
+
+                        if (wheel.IsGrounded)
+                        {
+                            float idlerMultiplier = wheel.IsIdler ? 0.3f : 1f;
+                            rig.AddForceAtPosition(invertValue * currentSteerForce * idlerMultiplier * steerInput * rig.transform.right,
+                                wheel.HitInfo.Point, ForceMode.Force);
+                        }
+                    }
+                }
+            }
+        }
+
+        private float GetSteerForceMultiplier()
+        {
+            var currentPair = suspensionController.CurrentFrictionPair;
+
+            if (suspensionController.HorizontalAngle >= currentPair.HorizontalAnglesRange.Max)
+            {
+                return 0.1f;
+            }
+
+            return currentPair.IsDefaultLayer ? steerForce : steerForce * currentPair.SteeringMultiplier;
+        }
+    }
+}
